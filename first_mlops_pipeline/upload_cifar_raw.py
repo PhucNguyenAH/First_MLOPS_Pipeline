27cc8c7a299bab6db3ce818a358ac4a249d41faa
AHPhucNguyen <hphuc4499@gmail.com>
2025-03-04 00:08:34 +1100

    fix matplotlib error

--- first_mlops_pipeline/upload_cifar_raw.py
+++ first_mlops_pipeline/upload_cifar_raw.py
@@ -5,6 +5,7 @@ import numpy as np
 from clearml import Dataset
 from tensorflow.keras.datasets import cifar10
 
+os.environ["MPLBACKEND"] = "TkAgg"
 
 def save_numpy_arrays(data, labels, data_filename, labels_filename):
     import argparse
